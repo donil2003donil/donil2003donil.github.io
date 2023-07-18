c88158da0e924d4dc9cf81e946a53749dda70d72
daanilka <dendraj3@gmail.com>
2023-07-19 02:16:36 +0300

    add second styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,6 @@ let prevButton = document.getElementsByClassName("information__menu-button")[0];
 const buttonPressed = (e) => {
     if(e.target.nodeName === 'BUTTON') {
         e.target.classList.add('information__menu-button_active');
-        console.log(prevButton);
         if(prevButton !== null) {
             prevButton.classList.remove('information__menu-button_active');
         }
@@ -11,3 +10,29 @@ const buttonPressed = (e) => {
     }
 }
 buttonGroup.addEventListener("click", buttonPressed);
+
+
+let burgerWrapper = document.getElementById('burger-icon');
+let burger = document.getElementsByClassName('header__burger-menu')[0];
+let navigationMenu = document.getElementsByClassName('nav')[0];
+let body = document.getElementById("body");
+
+burgerWrapper.addEventListener("click", function (e){
+    burger.classList.toggle('burger_active');
+    navigationMenu.classList.toggle('nav_active');
+    body.classList.toggle('menu_active')
+});
+
+
+let minusUnit = document.getElementsByClassName('product__quantity_plus')[0];
+let plusUnit = document.getElementsByClassName('product__quantity_minus')[0];
+let countUnit = Number(document.getElementsByClassName('product__quantity_show')[0].value);
+
+minusUnit.onclick = (e) => {
+    if (countUnit > 1) {
+        document.getElementsByClassName('product__quantity_show')[0].value = --countUnit;
+    }
+}
+plusUnit.onclick = (e) => {
+    document.getElementsByClassName('product__quantity_show')[0].value = ++countUnit;
+}
